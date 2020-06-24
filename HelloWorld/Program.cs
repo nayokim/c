e048c18399f3cdd3a4098f7c6a46657007c6a485
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-06-24 15:45:05 -0500

    data-types

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your favorite number!: ");
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your favorite number is: " + faveNumber);
+        }
+    }
+}
